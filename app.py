0f0124ebed31ad5e10ffb4ef76fa1b7940ed5556
Mr Hafeez <mrhafeez.ysrfau@gmail.com>
2025-06-23 05:25:04 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import string
+
+st.title("Word Count Dictionary Generator")
+
+st.write(
+    """
+    Paste or type your paragraph below.  
+    Click **Get Word Counts** to see how many times each word appears!
+    """
+)
+
+paragraph = st.text_area("Enter your paragraph here:")
+
+if st.button("Get Word Counts"):
+    if paragraph.strip():
+        translator = str.maketrans('', '', string.punctuation)
+        cleaned = paragraph.translate(translator).lower()
+        words = cleaned.split()
+        word_counts = {word: words.count(word) for word in set(words)}
+        st.write(word_counts)
+    else:
+        st.warning("Please enter some text.")
