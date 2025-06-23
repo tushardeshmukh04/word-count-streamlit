eea17d988d61851c380f99fb49d5532957fecd99
tushardeshmukh04 <147343993+tushardeshmukh04@users.noreply.github.com>
2025-06-23 12:32:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import string
 
-st.title("Word Count Dictionary Generator - By MrHafeez (for Demonstartion)")
+st.title("Word Count Dictionary Generator - By MrTushar (for Demonstartion)")
 
 st.write(
     """
